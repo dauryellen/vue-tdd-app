1b80b85d580565d17e1218da564d83a1999e172e
dauryellen <dauryellen@live.com>
2021-02-12 10:45:00 -0300

    Registrando os novos componentes na USerView

--- src/views/UserView.vue
+++ src/views/UserView.vue
@@ -1,10 +1,20 @@
 <template>
-  <div>UserView</div>
+  <div>
+    <v-user-search-form />
+    <v-user-profile />
+  </div>
 </template>
 
 <script>
+import VUserSearchForm from "../components/VUserSearchForm";
+import VUserProfile from "../components/VUserProfile";
+
 export default {
   name: "UserView",
+  components: {
+    VUserSearchForm,
+    VUserProfile,
+  },
 };
 </script>
 
