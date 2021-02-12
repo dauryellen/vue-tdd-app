6afd52c2b030a9dae994029222d1ef7d96644a65
dauryellen <dauryellen@live.com>
2021-02-12 10:32:48 -0300

    teste que verifica a existência dos componentes

--- tests/unit/UserView.spec.js
+++ tests/unit/UserView.spec.js
@@ -1,5 +1,7 @@
 import { shallowMount } from "@vue/test-utils";
 import UserView from "../../src/views/UserView.vue";
+import VUserSearchForm from "../../src/components/VUserSearchForm";
+import VUserProfile from "../../src/components/VUserProfile";
 
 describe("UserView", () => {
   it("renders the component", () => {
@@ -8,5 +10,16 @@ describe("UserView", () => {
 
     //assert
     expect(wrapper.html()).toMatchSnapshot();
+  });
+
+  it("renders main child components", () => {
+    //arrange
+    const wrapper = shallowMount(UserView);
+    const userSearchForm = wrapper.find(VUserSearchForm);
+    const userProfile = wrapper.find(VUserProfile);
+
+    //assert
+    expext(userSearchForm.exists()).toBe(true);
+    expext(userProfile.exists()).toBe(true);
   });
 });
